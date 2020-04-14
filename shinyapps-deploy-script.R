f48f15f1919bdef6a64cdde0c398d88e7b5dfeb6
Tim Wilson <timwilson7@gmail.com>
2020-04-13 22:15:48 -0400

    Updated styles w/ an external stylesheet. Added a script for deploying to shinyapps.io. Fixes #3.

---
+++ shinyapps-deploy-script.R
@@ -0,0 +1,11 @@
+# Use this script -- or some tweaked version of it -- to push the code
+# to shinyapps.io. 
+library(rsconnect)
+
+# Deploy the apps. 
+deployApp(appFiles = c("visualize-speedtest-flex.Rmd","styles.css"),
+          appName = "speedtest-results",
+          appTitle = "SpeedTest Results",
+          forceUpdate = TRUE)
+
+
